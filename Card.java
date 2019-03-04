264087260ecfd719f0235ee1952f8a1197882cf7
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2019-03-03 19:53:14 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,7 +1,24 @@
+import java.util.StringTokenizer;
 
 public class Card 
 {
 	
-	
+
+	public String getNumber(String card) {
+		 
+		String[] result = card.split("\\s");
+		 String number = result[result.length-2];
+		 
+		//StringTokenizer st = new StringTokenizer(card);
+		 System.out.println(number);
+		 return number; 
+	}
+
+	public String getSuite(String card) {
+		String[] result = card.split("\\s");
+		String suite = result[result.length - 1];
+		System.out.println(suite);
+		return suite;
+	}
 	
 }
