87eff7646883cdaba03ff5ad7acb2f1c63bfb2d7
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2019-04-07 13:16:55 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -2,22 +2,44 @@ import java.util.StringTokenizer;
 
 public class Card 
 {
-	
+	private String cardValue;
+	private String cardSuite;
 
-	public String getNumber(String card) {
+	public Card(String valueData, String suiteData) 
+	{
+		cardValue = valueData;
+		cardSuite = suiteData;
+	}
+	
+	public String getNumber()
+	{
+		return cardValue;
+	}
+	
+	public String getSuite()
+	{
+		return cardSuite;
+	}
+	
+	public String getCard()
+	{
+		return cardValue + cardSuite;
+	}
+
+	public static String getNumber(String card) {
 		 
 		String[] result = card.split("\\s");
 		 String number = result[result.length-2];
 		 
 		//StringTokenizer st = new StringTokenizer(card);
-		 System.out.println(number);
+		 //System.out.println(number);
 		 return number; 
 	}
 
-	public String getSuite(String card) {
+	public static String getSuite(String card) {
 		String[] result = card.split("\\s");
 		String suite = result[result.length - 1];
-		System.out.println(suite);
+		//System.out.println(suite);
 		return suite;
 	}
 	
